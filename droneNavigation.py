1edf5840b3c9fc981bf581a8c443cd8926e55b68
thegtabot <gtas321@gmail.com>
2024-10-15 22:17:57 -0400

    Update droneNavigation.py

--- droneNavigation.py
+++ droneNavigation.py
@@ -1,19 +1,36 @@
 from dronekit import Command, LocationGlobalRelative
 
 
-def navigate_to(lat, lon, alt)
+def navigate_to(coords)
     cmds = vehicle.commands
     cmds.clear()
 
-    # Add multiple waypoints
-    deliveryStack = [
-        [lat, lon, alt]  # Delivery points append here
-                             # Add more waypoints as needed
-    ]
+    # Add waypoints
+    deliveryStack = []
+         # Delivery points append here              
+         # Add more waypoints as needed
 
-    for lat, lon, alt in deliveryStack:
-        wp = Command(0, 0, 0, Command.WAYPOINT, 0, 0, 0, 0, 0, 0, lat, lon, alt)
+    deliveryStack.append(coords)
+    
+    #0 is lat 
+    #1 is lon
+    #2 is alt
+    for deliveryStack[0], deliveryStack[1], deliveryStack[2] in deliveryStack: 
+        wp = Command(0, 0, 0, Command.WAYPOINT, 0, 0, 0, 0, 0, 0, deliveryStack[0], deliveryStack[1], deliveryStack[2])
         cmds.add(wp)
 
     cmds.upload()
     vehicle.mode = VehicleMode("AUTO")
+
+'''
+from dronekit import VehicleMode, LocationGlobalRelative
+
+# Set the vehicle to GUIDED mode for autonomous control
+vehicle.mode = VehicleMode("GUIDED")
+
+# Define the target GPS coordinates (latitude, longitude, altitude)
+target_location = LocationGlobalRelative(latitude, longitude, altitude)
+
+# Command the drone to fly to the target location
+vehicle.simple_goto(target_location)
+'''
