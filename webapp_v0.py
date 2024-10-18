b22427b5805002a04ca6104d1d624b47f686b6f4
thegtabot <gtas321@gmail.com>
2024-10-18 02:15:14 -0400

    added a logo
    add logo.

--- webapp_v0.py
+++ webapp_v0.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 
 def index():
-    return "System Test"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
