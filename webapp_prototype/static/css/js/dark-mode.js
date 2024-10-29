8fb0e6085c0be045b6b0da35c1694d94fc6b4957
thegtabot <gtas321@gmail.com>
2024-10-29 14:14:56 -0400

    hotfix to login page

--- webapp_prototype/static/css/js/dark-mode.js
+++ webapp_prototype/static/css/js/dark-mode.js
@@ -1,15 +1,6 @@
 function toggleDarkMode() {
+    // Toggle dark mode on the body
     document.body.classList.toggle('dark-mode');
-    const flashMessages = document.querySelectorAll('.flash-message');
-    flashMessages.forEach(msg => {
-        msg.classList.toggle('dark-mode');
-    });
-    const pElements = document.querySelectorAll('p');
-    pElements.forEach(p => {
-        p.classList.toggle('dark-mode');
-    });
-    const inputFields = document.querySelectorAll('.input-group input');
-    inputFields.forEach(input => {
-        input.classList.toggle('dark-mode'); // Added for input fields
-    });
 }
+
+window.toggleDarkMode = toggleDarkMode;
